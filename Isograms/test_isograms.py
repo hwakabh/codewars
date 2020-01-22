e51310d379c47294c031edc1b55f0ce45004dcd8
hwakabh <hrykwkbys1024@gmail.com>
2020-01-23 01:23:53 +0900

    Modified test code for isogram.py with parameterized testings.

--- Isograms/test_isograms.py
+++ Isograms/test_isograms.py
@@ -3,11 +3,22 @@ from isograms import is_isogram
 
 class TestIsogram(unittest.TestCase):
     # Test class of isograms.py
+
     def test_is_isogram(self):
-        test_value = 'Dermatoglyphics'
-        expected = True
-        actual = is_isogram(string=test_value)
-        self.assertEqual(expected, actual)
+        test_patterns = [
+            ('Dermatoglyphics', True),   #-> Expected True
+            ('isogram', True),   #-> Expected True
+            ('aba', False),   #-> Expected False (duplicated with 'a')
+            ('moOse', False),   #-> Expected False (duplicated with 'o'/'O')
+            # ('Aa', True),  #-> Wrong Case
+            ('isIsogram', False),  #-> Expected False (duplicated with 'i'/'I')
+            ('', True),  #-> Expected True (empty string is valid isogram)
+        ]
+
+        for input_value, expected in test_patterns:
+            # print('Testing with paramter {0} : expected {1}'.format(input_value, expected))
+            with self.subTest(input_value=input_value, expected=expected):
+                self.assertEqual(is_isogram(string=input_value), expected)
 
 if __name__ == '__main__':
     unittest.main()
