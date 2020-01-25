76f45dd2475d8c6fa8c0ec0c12c444180e00b1b9
hwakabh <hrykwkbys1024@gmail.com>
2020-01-25 18:12:30 +0900

    Pushed test code for ComplementaryDNA.

---
+++ ComplementaryDna/test_complementary_dna.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest import main
+
+from complementary_dna import DNA_stand
+
+
+class TestComplementaryDna(TestCase):
+    # Test class of complementary_dna
+    def test_DNA_stand(self):
+        test_patterns = [
+            ('AAAA', 'TTTT'),
+            ('ATTGC', 'TAACG'),
+            ('GTAT', 'CATA'),
+            # ('GGCC', 'GGCC') #-> Wrong Case
+        ]
+
+        for d, expected in test_patterns:
+            with self.subTest(d=d, expected=expected):
+                self.assertEqual(DNA_stand(dna=d), expected)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
