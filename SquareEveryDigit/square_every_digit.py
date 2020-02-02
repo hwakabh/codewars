c0a54c3a91b0b735bcda7c3e4ffd5c2570d2670c
hwakabh <hrykwkbys1024@gmail.com>
2020-02-03 01:56:50 +0900

    Refactored solutions for square_every_digits.py with removing checks of data-types.

--- SquareEveryDigit/square_every_digit.py
+++ SquareEveryDigit/square_every_digit.py
@@ -2,16 +2,14 @@ import sys
 
 
 def square_digits(num):
-    if isinstance(num, int):
-        num = str(num)
     ans = ''
-    for n in num:
+    for n in str(num):
         ans += str(int(n) ** 2)
     return int(ans)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        print(square_digits(num=sys.argv[1]))
+        print(square_digits(num=int(sys.argv[1])))
     else:
         sys.exit(1)
