39e3d97602f9af59f54b2afa87f14c09dddc699e
hwakabh <hrykwkbys1024@gmail.com>
2020-02-02 22:16:13 +0900

    Pushed solutions for very_even_numbers.py

---
+++ VeryEvenNumbers/very_even_numbers.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def is_very_even_number(n):
+    while True:
+        sum_digit = 0
+        for d in str(n):
+            sum_digit += int(d)
+        if sum_digit >= 10:
+            n = sum_digit
+        else:
+            break
+    if sum_digit % 2 == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        print(is_very_even_number(n=int(sys.argv[1])))
+    else:
+        sys.exit(1)
