00f24423c1f6d6422e26396687024d41a72f0c73
hwakabh <hrykwkbys1024@gmail.com>
2020-02-13 13:27:55 +0900

    Pushed solutions and testcodes for break_camel_case.

---
+++ BreakCamelCase/test_break_camel_case.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest import main
+
+from break_camel_case import solution
+
+
+class TestBreakCamelCase(TestCase):
+    # Test class of break_camel_case
+    def test_solution(self):
+        test_patterns = [
+            ('helloWorld', 'hello World'),
+            ('camelCase', 'camel Case'),
+            ('breakCamelCase', 'break Camel Case'),
+        ]
+        for s, expected in test_patterns:
+            with self.subTest(s=s, expected=expected):
+                self.assertEqual(solution(s=s), expected)
+
+
+if __name__ == '__main__':
+    main(verbosity=2)
