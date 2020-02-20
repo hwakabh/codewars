998756a156dce9e428fbd6cf5006a868f11dee92
hwakabh <hrykwkbys1024@gmail.com>
2020-02-21 01:20:27 +0900

    [WIP]Pushed solutions for how_many_urinals_are_free.

---
+++ HowManyUrinalsAreFree/test_how_many_urials_are_free.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from unittest import main
+
+from how_many_urinals_are_free import get_free_urinals
+
+
+class TestHowManyUrinalsAreFree(TestCase):
+    def test_ge_free_urinals(self):
+        test_patterns = [
+            ("10001", 1),
+            ("1001", 0),
+            ("00000", 3),
+            ("0000", 2),
+            ("01000", 1),
+            ("00010", 1),
+            ("10000", 2),
+            ("0100000", 2),
+            ("1", 0),
+            ("0", 1),
+            ("10", 0),
+        ]
+        for u, expected in test_patterns:
+            with self.subTest(u=u, expected=expected):
+                self.assertEqual(get_free_urinals(urinals=u), expected)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
