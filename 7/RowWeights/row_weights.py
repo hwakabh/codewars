6525652c3649a98e2353c495ebb1655fc4bce5bf
hwakabh <hrykwkbys1024@gmail.com>
2020-02-23 11:47:12 +0900

    Pushed solutions and testcodes for row_weights.

---
+++ 7/RowWeights/row_weights.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def row_weights(array):
+    o = 0
+    e = 0
+    for i, n in enumerate(array):
+        if i % 2 == 0:
+            o += n
+        else:
+            e += n
+    return (o, e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        numbers = [int(i) for i in input('>>> Enter numbers to examine with comma-separated: ')]
+        print(row_weights(array=numbers))
+    else:
+        sys.exit(1)
