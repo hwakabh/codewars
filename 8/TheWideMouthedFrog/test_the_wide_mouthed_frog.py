44af816b578e0c6a0d1eaf659955389f5732b781
hwakabh <hrykwkbys1024@gmail.com>
2020-02-25 11:27:11 +0900

    Pushed solutions and testcodes for the_wide_mouthed_frog.

---
+++ 8/TheWideMouthedFrog/test_the_wide_mouthed_frog.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest import main
+
+from the_wide_mouthed_frog import mouth_size
+
+
+class TestTheWideMouthedFrog(TestCase):
+    def test_mouth_size(self):
+        test_patterns = [
+            ('toucan', 'wide'),
+            ('ant bear', 'wide'),
+            ('alligator', 'small'),
+        ]
+        for n, exp in test_patterns:
+            with self.subTest(n=n, exp=exp):
+                self.assertEqual(mouth_size(animal=n), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
