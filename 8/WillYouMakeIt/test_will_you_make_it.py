61ad788f5443e736d5087c4ccb15a961806f0dc6
hwakabh <hrykwkbys1024@gmail.com>
2020-02-27 19:12:18 +0900

    Pushed solutions and testcodes for will_you_make_it.

---
+++ 8/WillYouMakeIt/test_will_you_make_it.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from unittest import main
+
+from will_you_make_it import zero_fuel
+
+
+class TestWillYouMakeIt(TestCase):
+    def test_zero_fuel(self):
+        test_patterns = [
+            (50, 25, 2, True),
+            (100, 50, 1, False),
+        ]
+        for d, m, f, exp in test_patterns:
+            with self.subTest(d=d, m=m, f=f, exp=exp):
+                self.assertEqual(zero_fuel(distance_to_pump=d, mpg=m, fuel_left=f), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
