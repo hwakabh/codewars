04971de4200068b88ec05a77b3ff6dd61e2d26c2
hwakabh <hrykwkbys1024@gmail.com>
2020-03-08 23:50:29 +0900

    Pushed solutions and testcodes for what_is_the_real_floor.

---
+++ 8/WhatIsTheRealFloor/test_what_is_the_real_floor.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest import main
+
+from what_is_the_real_floor import get_real_floor
+
+
+class TestWhatIsTheRealFloor(TestCase):
+    def test_get_real_floor(self):
+        test_patterns = [
+            (1, 0),
+            (5, 4),
+            (15, 13),
+        ]
+        for inp, exp in test_patterns:
+            with self.subTest(inp=inp, exp=exp):
+                self.assertEqual(get_real_floor(n=inp), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
