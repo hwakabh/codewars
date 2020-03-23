e4432605b528336ecf14e504834ee1be96f037db
hwakabh <hrykwkbys1024@gmail.com>
2020-03-23 23:43:33 +0900

    Pushed solutions and testcodes for persistent_bugger.

---
+++ 6/PersistentBugger/test_persistence_bugger.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest import main
+
+from persistence_bugger import persistence
+
+
+class TestPersistenceBugger(TestCase):
+    def test_persistence(self):
+        test_patterns = [
+            (39, 3),
+            (4, 0),
+            (25, 2),
+            (999, 4),
+        ]
+        for inp, exp in test_patterns:
+            with self.subTest(inp=inp, exp=exp):
+                self.assertEqual(persistence(n=inp), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
