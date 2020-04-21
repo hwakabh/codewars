35f15a2c682a9e15435148285b8a0beb4392e374
hwakabh <hrykwkbys1024@gmail.com>
2020-04-22 05:00:04 +0900

    Refactored solutions for halving_sum with recursions.

--- 7/HalvingSum/halving_sum.py
+++ 7/HalvingSum/halving_sum.py
@@ -2,15 +2,10 @@ import sys
 
 
 def halving_sum(n):
-    if n <= 2:
+    if n == 1:
         return 1
     else:
-        s = n
-        d = 2
-        while d <= n // 2:
-            s += n // d
-            d *= 2
-        return s + 1
+        return n + halving_sum(n // 2)
 
 
 if __name__ == "__main__":
