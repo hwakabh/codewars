38d12822d0d68d2755bef00ab65ff2cbec887061
hwakabh <hrykwkbys1024@gmail.com>
2020-04-23 06:05:21 +0900

    Refactored solutions for pokemon_damage_calculator.

--- 6/PokemonDamageCalculator/pokemon_damage_calculator.py
+++ 6/PokemonDamageCalculator/pokemon_damage_calculator.py
@@ -2,32 +2,20 @@ import sys
 
 
 def calculate_damage(your_type, opponent_type, attack, defense):
+    m = {'fire': 'grass', 'water': 'fire', 'grass': 'water', 'electric': 'water'}
     e = 1
     if your_type == opponent_type:
         e = 0.5
 
-    if your_type == 'fire':
-        if opponent_type == 'grass':
-            e = 2
-        elif opponent_type == 'water':
-            e = 0.5
-    elif your_type == 'water':
-        if (opponent_type == 'grass') or (opponent_type == 'electric'):
-            e = 0.5
-        elif opponent_type == 'fire':
-            e = 2
-    elif your_type == 'grass':
-        if opponent_type == 'fire':
-            e = 0.5
-        elif opponent_type == 'water':
-            e = 2
-    elif your_type == 'electric':
-        if opponent_type == 'water':
-            e = 2
+    if m[your_type] == opponent_type:
+        e = 2
+    elif m[opponent_type] == your_type:
+        e = 0.5
+
     return 50 * (attack / defense) * e
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) == 1:
         types = input('>>> Enter types of your/opponent with comma-separated [fire/water/electric/grass]: ').split(',')
         if len(types) != 2:
