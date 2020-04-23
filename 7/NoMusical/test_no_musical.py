f4778a06e8e9f83908f8e6c2b2f0b32eded58ead
hwakabh <hrykwkbys1024@gmail.com>
2020-04-23 09:56:35 +0900

    [WIP]Pushed solutions and testcodes for no_musical.

---
+++ 7/NoMusical/test_no_musical.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest import main
+
+from no_musical import no_musical
+
+
+class TestNoMusical(TestCase):
+    def test_no_musical(self):
+        ptr = [
+            (2000, 2000, 100, 3, 0),
+            (2000, 3000, 0, 50, 1001),
+            (2000, 2020, 2, 4, 0),
+            (2000, 3000, 5, 2, 600),
+        ]
+        for s, e, m, d, exp in ptr:
+            with self.subTest(s=s, e=e, m=m, d=d, exp=exp):
+                self.assertEqual(no_musical(start_class=s, end_class=e, musical_performed_every=m, enrolment_duration=d), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
