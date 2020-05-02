493fe60c343d27001ef1a96d683b5e6cd548864f
hwakabh <hrykwkbys1024@gmail.com>
2020-05-02 09:08:55 +0900

    #3 Modified solutions with binary search instead of linear search, for ElementsEqualsItsIndex.

--- 6/ElementEqualsItsIndex/element_equals_its_index.py
+++ 6/ElementEqualsItsIndex/element_equals_its_index.py
@@ -2,9 +2,19 @@ import sys
 
 
 def index_equals_value(arr):
-    for i, x in enumerate(arr):
-        if arr[i] == i:
-            return x
+    l, h = 0, len(arr) - 1
+    while l <= h:
+        m = (l + h) // 2
+
+        if arr[m] < m:
+            l = m + 1
+        elif m < arr[m]:
+            h = m - 1
+        elif l != h:
+            h = m
+        else:
+            return m
+    # Case if not found
     return -1
 
 
