59afb3b0b8b713866264d6e3a2492291510e9cf4
hwakabh <hrykwkbys1024@gmail.com>
2020-05-15 04:52:52 +0900

    Pushed soltuions and testcodes for unique_string_characters.

---
+++ Python/7/UniqueStringCharacters/test_unique_string_characters.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest import main
+
+from unique_string_characters import solve
+
+
+class TestUniqueStringCharacters(TestCase):
+    def test_solve(self):
+        ptr = [
+            ('xyab', 'xzca', 'ybzc'),
+            ('xyabb', 'xzca', 'ybbzc'),
+            ('abcd', 'xyz', 'abcdxyz'),
+            ('xxx', 'xzca', 'zca'),
+        ]
+        for x, y, exp in ptr:
+            with self.subTest(x=x, y=y, exp=exp):
+                self.assertEqual(solve(a=x, b=y), exp)
+
+
+if __name__ == '__main__':
+    main(verbosity=2)
