6dfa72698385eb299585aabe008b73180e1d52df
hwakabh <hrykwkbys1024@gmail.com>
2020-05-23 21:59:11 +0900

    Pushed solutions and testcodes for take_the_derivative.

---
+++ Python/8/TakeTheDerivative/test_take_the_derivative.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from unittest import main
+
+from take_the_derivative import derive
+
+
+class TestTakeTheDerivative(TestCase):
+    def test_derive(self):
+        ptr = [
+            (7, 8, '56x^7'),
+            (5, 9, '45x^8'),
+        ]
+        for c, e, exp in ptr:
+            with self.subTest(c=c, e=e, exp=exp):
+                self.assertEqual(derive(coefficient=c, exponent=e), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
