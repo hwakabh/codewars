283427511186104b67813145366c3f53f16b3f1e
hwakabh <hrykwkbys1024@gmail.com>
2020-05-23 21:43:52 +0900

    Pushed solutions and testcodes for greet_me.

---
+++ Python/7/GreetMe/test_greet_me.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest import main
+
+from greet_me import greet
+
+
+class TestGreetMe(TestCase):
+    def test_greet(self):
+        ptr = [
+            ('riley', 'Hello Riley!'),
+            ('molly', 'Hello Molly!'),
+            ('BILLY', 'Hello Billy!'),
+        ]
+        for inp, exp in ptr:
+            with self.subTest(inp=inp, exp=exp):
+                self.assertEqual(greet(name=inp), exp)
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
