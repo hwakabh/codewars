38cd11575a507b9b44244a235a80928588ba10b5
hwakabh <hrykwkbys1024@gmail.com>
2020-05-28 08:50:15 +0900

    Pushed solutions and testcodes for array_plus_array.js.

---
+++ node/8/ArrayPlusArray/array_plus_array.js
@@ -0,0 +1,12 @@
+function arrayPlusArray(arr1, arr2) {
+    var sum = 0;
+    arr1.forEach(function(i) {
+        sum += i;
+    });
+    arr2.forEach(function(j) {
+        sum += j;
+    });
+    return sum;
+}
+
+module.exports = arrayPlusArray;
