dc339940fdc77da1f5f4fa52996334e1a5012f97
hwakabh <hrykwkbys1024@gmail.com>
2021-04-29 15:02:03 +0900

    Modified intents from 4 to 2.

--- node/7/GreetMe/test_greet_me.js
+++ node/7/GreetMe/test_greet_me.js
@@ -2,14 +2,14 @@ const assert = require("assert").strict;
 var func = require("./greet_me");
 
 let ptr = [
-    // Each test pattern
-    {"JACK": "Hello Jack!"},
-    {"ricky": "Hello Ricky!"}
+  // Each test pattern
+  {"JACK": "Hello Jack!"},
+  {"ricky": "Hello Ricky!"}
 ];
 
 ptr.forEach(function(p) {
-    var inp = Object.keys(p)[0];
-    var exp = p[inp];
+  var inp = Object.keys(p)[0];
+  var exp = p[inp];
 
-    assert.equal(func(inp), exp);
+  assert.equal(func(inp), exp);
 })
