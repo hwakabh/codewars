4ec025e71bab60a56efef580f833f88d23738415
hwakabh <hrykwkbys1024@gmail.com>
2021-04-29 15:02:33 +0900

    Modified intents from 4 to 2.

--- node/8/ArrayPlusArray/test_array_plus_array.js
+++ node/8/ArrayPlusArray/test_array_plus_array.js
@@ -2,17 +2,17 @@ const assert = require("assert").strict;
 var func = require("./array_plus_array");
 
 let ptr = [
-    // Each test pattern: [inp1, inp2, exp]
-    [[1, 2, 3], [4, 5, 6], 21],
-    [[-1, -2, -3], [-4, -5, -6], -21],
-    [[0, 0, 0], [4, 5, 6], 15],
-    [[100, 200, 300], [400, 500, 600], 2100]
+  // Each test pattern: [inp1, inp2, exp]
+  [[1, 2, 3], [4, 5, 6], 21],
+  [[-1, -2, -3], [-4, -5, -6], -21],
+  [[0, 0, 0], [4, 5, 6], 15],
+  [[100, 200, 300], [400, 500, 600], 2100]
 ]
 
 // Parameterize Test
 ptr.forEach(function(p) {
-    var inp = func(p[0], p[1]);
-    var exp = p[2];
+  var inp = func(p[0], p[1]);
+  var exp = p[2];
 
-    assert.equal(inp, exp);
+  assert.equal(inp, exp);
 });
